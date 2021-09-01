d1f7ecfe25cd34144812f687e5ee59ca18182292
keremmyolcu <keremm.98@gmail.com>
2021-09-01 16:57:01 +0300

    reqwiretoname commit2

--- app/src/main/java/com/example/stork/Activity/BackendTest.java
+++ app/src/main/java/com/example/stork/Activity/BackendTest.java
@@ -1,13 +1,11 @@
 package com.example.stork.Activity;
 
-import android.widget.Toast;
-
-import com.example.stork.API.RequestWireToName.Request.Header;
-import com.example.stork.API.RequestWireToName.Request.Parameters;
-import com.example.stork.API.RequestWireToName.Request.Request;
-import com.example.stork.API.RequestWireToName.Request.SourceAccount;
-import com.example.stork.API.RequestWireToName.RequestWireToName;
-import com.example.stork.API.RequestWireToName.Response.Response;
+import com.example.stork.API.ProcessEftRequestToIban.Request.Header;
+import com.example.stork.API.SendFastMoneyOrder.Request.Parameters;
+import com.example.stork.API.SendFastMoneyOrder.Request.Request;
+import com.example.stork.API.SendFastMoneyOrder.Request.SourceAccount;
+import com.example.stork.API.SendFastMoneyOrder.Response.Response;
+import com.example.stork.API.SendFastMoneyOrder.SendFastMoneyOrder;
 import com.example.stork.Database.DatabaseUtil;
 import com.example.stork.Database.Models.SavedTransaction;
 import com.example.stork.Database.CallWrapper;
@@ -23,8 +21,8 @@ public class BackendTest {
     public void run() {
 
         //First test.
-        //readToDatabaseTest();
-        //testRequestWireToName();
+        readToDatabaseTest();
+        SendfastMoneyOrder();
 
     }
 
@@ -38,61 +36,58 @@ public class BackendTest {
         });
     }
 
-    /**
-     * Yeni test fonksiyonlarının tanımlara aşağıya, run() içinde çağırmayı unutmayın..
-     */
-
-    public void testRequestWireToName(){
+    private void SendfastMoneyOrder() {
         Services services = new Services();
+        com.example.stork.API.ProcessEftRequestToIban.Request.Header header = new Header("API7909c7de460b462aa1d","API","331eb5f529c74df2b800926b5f34b874","5252012362481156055");
+        Parameters params = new Parameters();
+        params.paymentReason="07";
+        params.paymentSource="M";
+        params.senderAccountNumber="TR690013400093772246300352";
+        params.senderIdentityNumber="88982218590";
+        params.receiverCode="0046";
+        params.receiverAccountNumber="TR300004600883888000386809";
+        params.receiverIdentityNumber="-";
+        params.senderAddress="-";
+        params.senderName="GLOREDHEL PRYSTELLA";
+        params.senderPassportNo="-";
+        params.senderBirthPlace="-";
+        params.senderBirthDate="-";
+        params.explanation="test";
+        params.kolasAddressType="T";
+        params.kolasAddressValue="5306512288";
+        params.amount=100;
+        SourceAccount source = new SourceAccount();
+        source.accountSuffix=352;
+        source.branchCode=9142;
+        source.customerNo=64473;
+        source.currencyCode="TRY";
+        source.iBANNo="TR690013400093772246300352";
+        params.sourceAccount=source;
+        params.receiverName="BTCHBYR EGELYF";
+        params.customerIdentity=101096290;
+        params.customerNo=64473;
 
-        SourceAccount sourceAccount = new SourceAccount();
-        sourceAccount.setAccountSuffix(355);
-        sourceAccount.setBranchCode(9142);
-        sourceAccount.setCustomerNo(3374562);
 
-        //System.out.println("OLDUUUUUUUUUAAAAAAAAAA11111");
 
-        Parameters parameters = new Parameters();
-        parameters.setDestinationBranchCode(9300);
-        parameters.setDestinationName("BEOR THE OLD");
-        parameters.setDestinationLastName("ANGUİLLİFORMES");
-        parameters.setDestinationPhoneNo("5468996863");
-        parameters.setDestinationMotherName("GUĞU ÇİÇEĞİ");
-        parameters.setDestinationFatherName("RAPHANUS RAPHANİSTRUM");
-        parameters.setDestinationBirthDate("1936-07-19 14:43:24.480");
-        parameters.setReceiverName("ali");
-        parameters.setForceDuplicate(true);
-        parameters.setCustomerNo(3374562);
-        parameters.setSourceAccount(sourceAccount);
-        parameters.setExplanation("test");
-        parameters.setAmount(1);
-
-        Header header = new Header("API7909c7de460b462aa1d","API","54dsaf4ds3h2g1jhg651d","a7f77648-dfb7-42ef-a4b8-db6b79c1cfab");
-
-        Request Request1 =  new Request(header,parameters);
-
-        RequestWireToName RetrofitClient = services.createRetrofit().create(RequestWireToName.class);
-
-        Call<Response> res = RetrofitClient.GetPostValue(Request1);
-        //System.out.println("OLDUUUUUUUUUAAAAAAAAAA2222");
+        Request request = new Request(header,params);
+        SendFastMoneyOrder Retrofit = services.createRetrofit().create(SendFastMoneyOrder.class);
+        Call<Response> res = Retrofit.GetPostValue(request);
         res.enqueue(new Callback<Response>() {
             @Override
             public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
-                //System.out.println("OLDUUUUUUUUUAAAAAAAAAA3333");
-                System.out.println("HTTP STATUS"+response.code());
-                if(response.body().getData()!=null){
-                    System.out.println(response.body().getData().getTransactionID());
-                }else{
-                    System.out.println("OLMADDDDIII!!!");
-                }
+                System.out.println("---------------");
+                System.out.println("CEVAPPPP "+response.code());
 
             }
 
             @Override
             public void onFailure(Call<Response> call, Throwable t) {
-                System.out.println(t.getCause()  +"FAILLLLLLLLLL!!!!!!!!!!");
+                System.out.println("hataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
             }
         });
     }
+    /**
+     * Yeni test fonksiyonlarının tanımlara aşağıya, run() içinde çağırmayı unutmayın..
+     */
 
 }
