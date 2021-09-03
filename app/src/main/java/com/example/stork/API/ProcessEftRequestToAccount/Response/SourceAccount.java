51686d07c2d677bfef837cc34ed164b53d355475
keremmyolcu <keremm.98@gmail.com>
2021-09-03 14:01:01 +0300

    ProcessEftRequestToAccount added

---
+++ app/src/main/java/com/example/stork/API/ProcessEftRequestToAccount/Response/SourceAccount.java
@@ -0,0 +1,21 @@
+package com.example.stork.API.ProcessEftRequestToAccount.Response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SourceAccount {
+
+    @SerializedName("$type")
+    @Expose
+    public String $type;
+    @SerializedName("AccountSuffix")
+    @Expose
+    public Integer accountSuffix;
+    @SerializedName("BranchCode")
+    @Expose
+    public Integer branchCode;
+    @SerializedName("CustomerNo")
+    @Expose
+    public Integer customerNo;
+
+}
