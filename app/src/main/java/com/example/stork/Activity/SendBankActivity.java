85e9527dbfb36b30bcda44b068b90fe2d36c84ff
penguin <e.ozbay75@gmail.com>
2021-09-06 15:44:04 +0300

    Kayitli Alicilar kismi calisiyor artik

--- app/src/main/java/com/example/stork/Activity/SendBankActivity.java
+++ app/src/main/java/com/example/stork/Activity/SendBankActivity.java
@@ -1,12 +1,16 @@
 package com.example.stork.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.stork.API.ProcessEftRequestToIban.Request.Header;
 import com.example.stork.API.SendFastMoneyOrder.Request.Parameters;
@@ -14,28 +18,64 @@ import com.example.stork.API.SendFastMoneyOrder.Request.Request;
 import com.example.stork.API.SendFastMoneyOrder.Request.SourceAccount;
 import com.example.stork.API.SendFastMoneyOrder.Response.Response;
 import com.example.stork.API.SendFastMoneyOrder.SendFastMoneyOrder;
+import com.example.stork.Adapter.RecyclerAdapter;
+import com.example.stork.Database.CallWrapperCustomer;
+import com.example.stork.Database.DatabaseUtil;
+import com.example.stork.Database.Models.SavedCustomer;
 import com.example.stork.R;
 import com.example.stork.Services;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 
 public class SendBankActivity extends AppCompatActivity {
+    private ArrayList<SavedCustomer> savedList;
+    private RecyclerView rview;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_bank);
-        Button new_adress=findViewById(R.id.new_adress);
 
-        new_adress.setOnClickListener(new View.OnClickListener(){
+        Button new_address=findViewById(R.id.new_adress);
+
+        savedList = new ArrayList<>();
+        rview = findViewById(R.id.recyclerView);
+        adapter = new RecyclerAdapter(savedList, this, new RecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(SavedCustomer item) {
+                Toast.makeText(getApplicationContext(), "Eyyy "+ item.getName()+ " sen kimsin!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rview.setAdapter(adapter);
+
+        new_address.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 startActivity(new Intent(SendBankActivity.this,MoneyTransferActivity.class));
             }
         });
-
     }
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DatabaseUtil db = new DatabaseUtil();
+        savedList.clear();
+        db.readCustomerData(new CallWrapperCustomer() {
+            @Override
+            public void readCustomerDataCallback(List<SavedCustomer> customers) {
+                for(SavedCustomer sc : customers){
+                    savedList.add(new SavedCustomer(sc.getName(),sc.getIBAN(),sc.getPhotoLink()));
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
 
 }
