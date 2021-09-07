5023f472f11bccb1e798d616f0c739aac0c51362
Yigit Demirko <46991541+YigitDemirko@users.noreply.github.com>
2021-09-07 14:10:47 +0300

    bug fixed

--- app/src/main/java/com/example/stork/Activity/PaymentsActivity.java
+++ app/src/main/java/com/example/stork/Activity/PaymentsActivity.java
@@ -11,12 +11,21 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PaymentsActivity extends AppCompatActivity {
     private FloatingActionButton groups;
+    private FloatingActionButton talimat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payments);
 
         groups = findViewById(R.id.groups_fb);
+        talimat = findViewById(R.id.floatingActionButton9);
+
+        talimat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PaymentsActivity.this,TalimatlarActivity.class));
+            }
+        });
 
         groups.setOnClickListener(new View.OnClickListener() {
             @Override
