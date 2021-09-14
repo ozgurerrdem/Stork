932e3ac8d0da88d2c8528a455df5869859690680
Duygusys <duygusys@icloud.com>
2021-09-14 16:23:42 +0300

    profile page

--- app/src/main/java/com/example/stork/Activity/ProfilActivity.java
+++ app/src/main/java/com/example/stork/Activity/ProfilActivity.java
@@ -12,5 +12,6 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
     }
 }
